c1cd6703846024eda1cb34963c957dbcb461aca0
Aman Pratap Singh <amanpratapsingh030@gmail.com>
2026-06-30 21:09:08 +0530

    Added Basic Routing Layer.

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,15 @@ import express from 'express';
 
 import connectDB from './config/dbConfig.js';
 import { PORT } from './config/serverConfig.js';
+import apiRouter from './routes/index.js';
 
 
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+
+app.use('/api', apiRouter);
 
 app.get('/ping', (req, res) => {
   return res.status(200).json({
