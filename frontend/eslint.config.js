3f7d60d4eb2072c83d2488504b09e811ee23e72e
Aman Pratap Singh <amanpratapsingh030@gmail.com>
2026-07-10 09:46:13 +0530

    ShadCn Setup and Eslint Configuration Added.

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,8 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -17,5 +18,16 @@ export default defineConfig([
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'react/prop-types': 'off',
+      'react-refresh/only-export-components': [ 'warn', { allowConstantExport: true } ],
+    },
   },
-])
+]);
