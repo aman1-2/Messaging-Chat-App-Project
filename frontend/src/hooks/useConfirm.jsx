468ebf2f749e92cecf4f2d00d8a38318a3c87f13
Aman Pratap Singh <amanpratapsingh030@gmail.com>
2026-07-16 21:26:01 +0530

    Confirm modal and the function with dialog box logic implemented.

--- frontend/src/hooks/useConfirm.jsx
+++ frontend/src/hooks/useConfirm.jsx
@@ -1,54 +1,54 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 
-// import { Button } from '@/components/ui/button';
-// import { Dialog, DialogContent, DialogDescription, DialogFooter,DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import { Dialog, DialogContent, DialogDescription, DialogFooter,DialogHeader, DialogTitle } from '@/components/ui/dialog';
 
-// const useConfirm = ({ title, message }) => {
-//     const [promise, setPromise] = useState(null);
+const useConfirm = ({ title, message }) => {
+    const [promise, setPromise] = useState(null);
 
-//     async function confirmation() {
-//         return new Promise((resolve) => {
-//             setPromise({resolve});
-//         });
-//     }
+    async function confirmation() {
+        return new Promise((resolve) => {
+            setPromise({resolve});
+        });
+    }
 
-//     const handleClose = () => {
-//         setPromise(null);
-//     };
+    const handleClose = () => {
+        setPromise(null);
+    };
 
-//     const handleConfirm = () => {
-//         promise?.resolve(true);
-//         handleClose();
-//     };
+    const handleConfirm = () => {
+        promise?.resolve(true);
+        handleClose();
+    };
 
-//     const ConfirmDialog = () => {
-//         return (
-//             <Dialog open={(promise !== null)} onOpenChange={handleClose}>
-//                 <DialogContent>
-//                     <DialogHeader>
-//                         <DialogTitle>
-//                             {title}
-//                         </DialogTitle>
-//                         <DialogDescription>
-//                             {message}
-//                         </DialogDescription>
-//                     </DialogHeader>
+    const ConfirmDialog = () => {
+        return (
+            <Dialog open={(promise !== null)} onOpenChange={handleClose}>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>
+                            {title}
+                        </DialogTitle>
+                        <DialogDescription>
+                            {message}
+                        </DialogDescription>
+                    </DialogHeader>
 
-//                     <DialogFooter>
-//                         <Button onClick={handleClose} variant="secondary">
-//                             Cancel
-//                         </Button>
+                    <DialogFooter>
+                        <Button onClick={handleClose} variant="secondary">
+                            Cancel
+                        </Button>
 
-//                         <Button onClick={handleConfirm}>
-//                             Confirm
-//                         </Button>
-//                     </DialogFooter>
-//                 </DialogContent>
-//             </Dialog>
-//         );
-//     };
+                        <Button onClick={handleConfirm}>
+                            Confirm
+                        </Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        );
+    };
 
-//     return { ConfirmDialog, confirmation };
-// };
+    return { ConfirmDialog, confirmation };
+};
 
-// export default useConfirm;
+export default useConfirm;
