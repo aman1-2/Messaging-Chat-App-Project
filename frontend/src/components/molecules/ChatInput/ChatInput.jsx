5ce3e31d0e70e17df8146e18d51c2a179c1957bb
Aman Pratap Singh <amanpratapsingh030@gmail.com>
2026-07-24 02:26:36 +0530

    Fixed the logic of S3 AWS upload on frontend.

--- frontend/src/components/molecules/ChatInput/ChatInput.jsx
+++ frontend/src/components/molecules/ChatInput/ChatInput.jsx
@@ -1,37 +1,42 @@
-// import { getPresignedUrlRequest, uploadeImageToAWSPresignedUrl } from '@/apis/s3';
+import { useQueryClient } from '@tanstack/react-query';
+
+import { getPresignedUrlRequest, uploadeImageToAWSPresignedUrl } from '@/apis/s3';
 import Editor from '@/components/atoms/Editor/Editor';
 import useAuth from '@/hooks/context/useAuth';
 import useCurrentWorkspace from '@/hooks/context/useCurrentWorkspace';
 import useSocket from '@/hooks/context/useSocket';
-// import { useQueryClient } from '@tanstack/react-query';
 
 const ChatInput = () => {
     const { socket, currentChannel } = useSocket();
     const { auth } = useAuth();
     const { currentWorkspace } = useCurrentWorkspace();
 
-    // const queryClient = useQueryClient();
+    const queryClient = useQueryClient();
 
     console.log('channel: ', currentChannel);
 
     async function handleSubmit({ messageBody, image }) {
         console.log(messageBody, image);
 
-        // if(image) {
-        //     const presignedUrl = await queryClient.fetchQuery({
-        //         queryKey: ['getPresignedUrl'],
-        //         queryFn: () => getPresignedUrlRequest({ token: auth?.token })
-        //     });
+        let fileUrl = null;
 
-        //     console.log(presignedUrl);
+        if(image) {
+            const presignedUrl = await queryClient.fetchQuery({
+                queryKey: ['getPresignedUrl'],
+                queryFn: () => getPresignedUrlRequest({ token: auth?.token })
+            });
 
-        //     const AWSResponse = await uploadeImageToAWSPresignedUrl(presignedUrl, image);
-        //     console.log('file upload success: ', AWSResponse);
-        // }
+            console.log(presignedUrl);
+
+            const AWSResponse = await uploadeImageToAWSPresignedUrl({url: presignedUrl.data, file: image});
+            console.log('file upload success: ', AWSResponse);
+            fileUrl = presignedUrl.split('?')[0];
+        }
 
         socket?.emit('NewMessage', {
             channelId: currentChannel,
             body: messageBody,
+            image: fileUrl,
             senderId: auth?.user?._id,
             workspaceId: currentWorkspace?.data?._id
         }, (data) => {
